fb22f8c7183bc8b637257eb38e239eab44c5f9ba
chijioke <superkingsely@gmail.com>
2024-05-12 05:51:11 -0700

    okay bs added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PortfolioComponent } from './main/portfolio/portfolio.component';
-import { PageNotFoundComponent } from './main/page-not-found/page-not-found.component';
-import { HomeComponent } from './main/home/home.component';
-import { ResumeComponent } from './main/resume/resume.component';
-import { ContactComponent } from './main/contact/contact.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'portfolios',
-    component: PortfolioComponent,
-  },
-  {
-    path: 'resumes',
-    component: ResumeComponent,
-  },
-  {
-    path: 'contact',
-    component: ContactComponent,
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent,
-  },
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { PortfolioComponent } from './main/portfolio/portfolio.component';
-import { MainComponent } from './main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './main/page-not-found/page-not-found.component';
-import { HomeComponent } from './main/home/home.component';
-import { ResumeComponent } from './main/resume/resume.component';
-import { ContactComponent } from './main/contact/contact.component';
-import { CardComponent } from './main/portfolio/card/card.component';
-import { ProjectsService } from './projects.service';
 import {ModalModule} from 'ngx-bootstrap/modal';
 import {CarouselModule} from 'ngx-bootstrap/carousel';
 import {CollapseModule} from 'ngx-bootstrap/collapse';
 import {AccordionModule} from 'ngx-bootstrap/accordion';
-import { ModalComponent } from './modal/modal.component'
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    PortfolioComponent,
-    MainComponent,
-    PageNotFoundComponent,
-    HomeComponent,
-    ResumeComponent,
-    ContactComponent,
-    CardComponent,
-    ModalComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -43,7 +24,7 @@ import { FormsModule } from '@angular/forms';
     CollapseModule.forRoot(),
     AccordionModule.forRoot()
   ],
-  providers: [ProjectsService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
